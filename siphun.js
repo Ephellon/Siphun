02f4534590b7342eeaa5e82a13b0d738cdcc1119
Ephellon <minkcbos@gmail.com>
2018-10-09 21:50:48 -0500

    Update siphun.js
    Added the `R` (Random) helper function. Siphun is far more secure now. The collision table is now manageable (under 5%?).

--- siphun.js
+++ siphun.js
@@ -2,26 +2,49 @@
  * @author Ephellon Dantzler (Cliffton E.D. Tucker)
  */
 
-function Siphun(string, fidelity) {
-  var array = (string += '').split(/([^]{16})/),
+function Siphun(string = '', fidelity = 0) {
+  let array = (string += '').split(/([^]{256})/),
       gamma = 0,
-      base,
-      method;
+      result = [],
+      method,
+      base;
 
-  fidelity = 33 - (((fidelity || 0) * 16) | 0);
+  let R = (t = '') => {
+    let s = [], S = [], N = 256, l = t.length;
 
-  method = function(s) {
-    return s? s.charCodeAt(0): s;
+    if(!l)
+      return '';
+    else
+      t = ' ' + t;
+
+    for(let n = 0; n < N;)
+      s.push(n++);
+
+    for(let n = 0, x = n, w = x; n < N; n++) {
+      x = (x + s[n] + t.charCodeAt(n % l)) % N;
+      s[w] = s[n];
+      s[n] = s[x];
+      s[x] = s[w];
+    }
+
+    for(let n = -1; n < N; n++)
+      S.push(t[s[n]] || '');
+
+    return S.join('');
   };
 
-  array.forEach(function(value, index, self) {
-    return (value == "")?
-      self.splice(index, 1):
-    gamma += value.charCodeAt(0);
-  });
+  fidelity = 33 - ((fidelity * 16) | 0);
 
-  for(var index = 0, length = array.length, last = length - 1, result = []; index < length; index++)
-    for(var self = array[index], next = (array[index + 1] || ""), mirror = array[last], a, b, c, d, e, f, g = gamma, i = 0, j = self.length, k = mirror.length, l = length, m = k - 1, q = fidelity; i < j; ++i, --m, g = gamma += a + b + c + d + e + f)
+  method = s => s? s.charCodeAt(0): s;
+
+  array.forEach((value, index, self = this) =>
+    (value == '')?
+      self.splice(index, 1):
+    gamma += value.charCodeAt(0)
+  );
+
+  for(let index = 0, length = array.length, last = length - 1; index < length; index++)
+    for(let self = array[index], next = (array[index + 1] || ""), mirror = array[last], a, b, c, d, e, f, g = gamma, i = 0, j = self.length, k = mirror.length, l = length, m = k - 1, q = fidelity; i < j; ++i, --m, g = gamma += a + b + c + d + e + f)
       a = method(self[i])         | 0,
       b = method(self[j - i - 1]) | 0,
       c = method(mirror[m])       | 0,
@@ -38,13 +61,13 @@ function Siphun(string, fidelity) {
       ));
 
   result.splice(fidelity, result.length - fidelity);
-  base = ((gamma % 20) + 16);
+  base = (gamma % 20) + 16;
 
-  result.forEach(function(value, index, self) {
-    return self.splice(index, 1, (value ^ gamma).toString(base));
-  });
+  result.forEach((value, index, self) => self.splice(index, 1, (value ^ gamma).toString(base)));
 
-  string = result = result.join("").slice(0, 256);
+  string = result = result.join('').slice(0, 256);
+  gamma = gamma.toString(base);
+  base = (base * fidelity).toString(base);
 
-  return result + gamma.toString(base) + base;
+  return R(result) + base + gamma;
 };
